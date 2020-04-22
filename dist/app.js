a8befd37b0592e035fda73ec224fd4b7b1e7cd23
Philip London <philip@blockchain.com>
2020-04-22 09:51:28 +0200

    Fix request data and headers

--- dist/app.js
+++ dist/app.js
@@ -6,22 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
 const cors_1 = __importDefault(require("cors"));
+const axios_curlirize_1 = __importDefault(require("axios-curlirize"));
 const app = express_1.default();
 const port = 3000;
 app.use(express_1.default.json());
 app.use(cors_1.default());
+axios_curlirize_1.default(axios_1.default);
 app.post("/api/v3/mobile_payments/card_details*/", (req, res) => {
-    console.log(req);
     const request = axios_1.default({
         method: 'POST',
         url: 'https://igw-demo.every-pay.com/api/v3/mobile_payments/card_details',
-        data: Object.assign({}, req.body),
+        data: Object.assign(Object.assign({}, req.body), { cc_details: JSON.parse(req.body.cc_details) }),
         headers: {
+            'Accept': 'application/json',
             'Content-Type': 'application/json',
             Authorization: req.headers.authorization
         }
     });
-    return request.then((data) => res.send(data)).catch((e) => { res.send(e); });
+    return request.then((data) => res.send(data.data)).catch((e) => { res.send(e); });
 });
 app.listen(port, err => {
     if (err) {
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,28 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import curlirize from 'axios-curlirize';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+curlirize(axios);
 
 app.post("/api/v3/mobile_payments/card_details*/", (req, res) => {
-  console.log(req)
   const request = axios({
     method: 'POST',
     url: 'https://igw-demo.every-pay.com/api/v3/mobile_payments/card_details',
-    data: { ...req.body },
+    data: { ...req.body, cc_details: JSON.parse(req.body.cc_details) },
     headers: {
+      'Accept': 'application/json',
       'Content-Type': 'application/json',
       Authorization: req.headers.authorization
     }
   })
 
-  return request.then((data) => res.send(data)).catch((e) => { res.send(e) })
+  return request.then((data) => res.send(data.data)).catch((e) => { res.send(e) })
   
 });
 
